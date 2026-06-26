4e3f5c14b14415a4c5ee20790b3316b565428128
Tami <tamigaertner@outlook.com>
2026-06-26 12:52:44 -0400

    Add Lambda function with SnapStart, X-Ray tracing, and live alias

---
+++ infrastructure/lambda.tf
@@ -0,0 +1,25 @@
+resource "aws_lambda_function" "cloud_audit_function"{
+    function_name = "CloudAuditFunction"
+    runtime       = "java21"
+    handler       = "io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler::handleRequest"
+    filename      = "../target/function.zip"
+    role          = aws_iam_role.lambda_exec_role.arn
+    memory_size   = 512
+    timeout       = 30
+    publish       = true
+
+    tracing_config {
+        mode = "Active"
+    }
+
+    snap_start {
+        apply_on = "PublishedVersions"
+    }
+
+}
+
+resource "aws_lambda_alias" "live"{
+    name             =  "live"
+    function_name    = aws_lambda_function.cloud_audit_function.function_name
+    function_version = aws_lambda_function.cloud_audit_function.version
+}
